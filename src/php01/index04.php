0ef09b49d571545c4423114426fe71a5de5dd546
ayaka y <color.scent.1127@gmail.com>
2024-03-07 03:09:40 +0900

    サンプルファイルの追加

---
+++ src/php01/index04.php
@@ -0,0 +1,45 @@
+<?php
+$a = 5;
+
+if( $a === 5 ){
+echo "\$aは5です";// 真の時に実行される処理
+}else{
+echo "\$aは5以外です";// 偽の時に実行される処理
+}
+
+
+
+// <?php
+// $a = 7;
+
+// if ($a === 5) {
+// echo "\$aは5です";
+// }elseif ($a === 7){
+// echo "\$aは7です";
+// }else{
+// echo "\$aは5と7以外です";
+// }
+
+
+
+// <?php
+// $people = "Saburo";
+
+// switch ($people) {
+// case "Taro":
+// echo "太郎です";
+// break;
+// case "Jiro":
+// echo "次郎です";
+// break;
+// case "Saburo":
+// echo "三郎です";
+// break;
+// }
+
+
+
+// <?php
+// $a = 7;
+// $b = ($a > 5) ? "TRUE" : "FALSE";
+// echo $b;
